755f32b8489416cb307d83697cec4dd25a268b93
littledogboy <littledogboy@126.com>
2016-10-27 01:54:16 +0800

    prictise3

--- RAC1_搭建/ViewController.m
+++ RAC1_搭建/ViewController.m
@@ -20,6 +20,12 @@
 static int GridXBlocks = 13;
 static int GridYBlocks = 7;
 
+typedef NS_ENUM(NSUInteger, SpiritState) {
+    SpiritStateAppear,
+    SpiritStateRunning,
+    SpiritStateDisappear,
+};
+
 
 @implementation ViewController
 
@@ -34,74 +40,196 @@ static int GridYBlocks = 7;
     [self.spirit startAnimating];
     
     // 方向坐标
-    RACSignal *stepSignal = @[RACTuplePack(@1, @0), RACTuplePack(@1, @0),
-                              RACTuplePack(@1, @0), RACTuplePack(@0, @1),
-                              RACTuplePack(@0, @1), RACTuplePack(@0, @1),
-                              RACTuplePack(@1, @0), RACTuplePack(@1, @0),
-                              RACTuplePack(@1, @0), RACTuplePack(@1, @0),
-                              RACTuplePack(@0, @-1), RACTuplePack(@0, @-1),
-                              RACTuplePack(@1, @0), RACTuplePack(@1, @0),
-                              RACTuplePack(@1, @0)
-                              ].rac_sequence.signal;
-    //  初始位置
+    NSArray *steps = @[RACTuplePack(@1, @0), RACTuplePack(@1, @0),
+                       RACTuplePack(@1, @0), RACTuplePack(@0, @1),
+                       RACTuplePack(@0, @1), RACTuplePack(@0, @1),
+                       RACTuplePack(@1, @0), RACTuplePack(@1, @0),
+                       RACTuplePack(@1, @0), RACTuplePack(@1, @0),
+                       RACTuplePack(@0, @-1), RACTuplePack(@0, @-1),
+                       RACTuplePack(@1, @0), RACTuplePack(@1, @0),
+                       RACTuplePack(@1, @0)
+                       ];
+    
     RACTuple *startBlock = RACTuplePack(@1, @2);
     
-    //  方向坐标 -> （实际坐标）
-    stepSignal =  [[[stepSignal scanWithStart:startBlock reduce:^id(RACTuple *last, RACTuple *direction) {
-        RACTupleUnpack(NSNumber *x1, NSNumber *y1) = last;
-        RACTupleUnpack(NSNumber *x2, NSNumber *y2) = direction;
-        NSNumber *x = @(x1.integerValue + x2.integerValue);
-        NSNumber *y = @(y1.integerValue + y2.integerValue);
-        return RACTuplePack(x, y); // 新信号中的值
-    }] startWith:startBlock] collect];
+    RACSequence *stepsSequence = steps.rac_sequence;
     
-    // 自动点击信号
-    RACSignal *autoClickSignal = [self.autoRunBtn rac_signalForControlEvents:UIControlEventTouchUpInside];
-    // 用户点击信号
-    RACSignal *oneStepClickSignal = [self.oneStepBtn rac_signalForControlEvents:UIControlEventTouchUpInside];
-    // 按时间 timer 信号
-    RACSignal *timerSignal = [RACSignal interval:0.5 onScheduler:[RACScheduler mainThreadScheduler]];
-    // 空信号（相当于有next值的一个占位符）
-    RACSignal *idSignal = [RACSignal return:nil];
+    NSInteger spiritCount = steps.count + 1; // 步数 + 1个起始位置
     
-    // 生阶操作，把点击返回值 --》 点击返回 信号
-    RACSignal *autoRunButtonClickSignal = [autoClickSignal mapReplace:timerSignal];
-    RACSignal *oneStepButtonClickSignal = [oneStepClickSignal mapReplace:idSignal];
-    // 两个信号之间的切换 switchToLatest, 切换到最近一个并阻断上一个
-    RACSignal *oneStepAutoSignal = [[autoRunButtonClickSignal merge:oneStepButtonClickSignal] switchToLatest];
+    void (^updateXYConstraints)(UIView *view, RACTuple *location) = ^(UIView *view, RACTuple *location) {
+        CGFloat width = self.grid.frame.size.width / GridXBlocks;
+        CGFloat height = self.grid.frame.size.height / GridYBlocks;
+        CGFloat x = [location.first floatValue] * width;
+        CGFloat y = [location.second floatValue] * height;
+        view.frame = CGRectMake(x, y, width, height);
+    };
     
-    RACSignal *spiritRunSignal = [[[[stepSignal concat:[RACSignal never]] sample:oneStepAutoSignal] scanWithStart:RACTuplePack(nil, @0) reduce:^id(RACTuple *value, NSArray *steps) {
-        // 每次获取的都是 一个 tuple 一个原来的array
-        // runing next
-        NSNumber *idx = value.second; // 当前下标值，从0开始
-        NSInteger nextIdx = (idx.integerValue + 1) % steps.count; // 下个下标
-        // 下个下标元素， 下个下标
-        return RACTuplePack(steps[nextIdx], @(nextIdx));
-    }] reduceEach:^id(NSArray *steps, id _){
-        return steps;
+    for (int i = 1; i < spiritCount + 1; ++i) {
+        UIImageView *spiritView = [[UIImageView alloc] init];
+        
+        spiritView.tag = i;
+        spiritView.animationImages = @[img1, img2, img3];
+        spiritView.animationDuration = 1.0;
+        spiritView.alpha = 0.0f;
+        [self.grid addSubview:spiritView];
+        
+        updateXYConstraints(spiritView, startBlock);
+    }
+    
+    
+   // stepSignal
+    RACSignal *stepSignal = [stepsSequence.signal scanWithStart:startBlock reduce:^id(RACTuple *running, RACTuple *next) {
+        RACTupleUnpack(NSNumber *x1, NSNumber *y1) = running;
+        RACTupleUnpack(NSNumber *x2, NSNumber *y2) = next;
+        return RACTuplePack(@(x1.integerValue + x2.integerValue),
+                            @(y1.integerValue + y2.integerValue));
+    }]; // 这里先不要 startWith :startBlock 因为第一步不需要延迟，做完延迟再拼接头
+    
+    // 每一秒挪动一步
+    stepSignal = [[stepSignal map:^id(id value) {
+        return [[RACSignal return:value] delay:1];
+    }] concat]; // map 只做了替换，但时间上没有变化，还是紧挨着。需要逐个拼接起来。
+    
+    // 起始信号 concat: run信号 ： 终止信号
+    
+    // 一个行走的信号好了，下面要发送多个行走信号。不同行走信号的tag值不同。写一个函数或者block封装起来。
+    // ps：idx 只起到tag值作用。一个头一个尾 就足够了
+    RACSignal *(^newSpiritSignal)(NSNumber *idx) = ^RACSignal *(NSNumber *idx) {
+       return [[[RACSignal return:RACTuplePack(idx,
+                                                                     @(SpiritStateAppear),
+                                                                     startBlock)] concat:
+                                      [stepSignal map:^id(RACTuple *xy) {
+            return RACTuplePack(idx, @(SpiritStateRunning), xy);
+        }]] concat:[RACSignal return:RACTuplePack(idx, @(SpiritStateDisappear), nil)]];
+        
+    };
+    
+    // 从 1 ~ spiritCount
+    // 创建一个1.5 间隔的信号，取 spiritCount 个
+    RACSignal *timerSignal = [[RACSignal interval:1.5 onScheduler:[RACScheduler mainThreadScheduler]] take:spiritCount];
+    // 把时间信号变换为数字信号
+    RACSignal *counterSignal = [timerSignal scanWithStart:@0 reduce:^id(NSNumber *num, id next) {
+        return @(num.integerValue + 1);
     }];
     
-    // 点击之后，才开始 1 秒间隔的连续信号 zip 组合最新的。
-    // 点击开始： take concat  停止：takeUntil skip 跳过第1个 再次点击的信号
-//    RACSignal *autoStepSignal =  [[[autoClickSignal take:1] concat:timerSignal] takeUntil:[autoClickSignal skip:1]];
+    //
+    RACSignal *autoBtnClickSignal = [[self.autoRunBtn rac_signalForControlEvents:UIControlEventTouchUpInside] mapReplace:@"A"];
+    RACSignal *oneStepClicSignal = [[self.oneStepBtn rac_signalForControlEvents:UIControlEventTouchUpInside] mapReplace:@"M"];
     
-    // 信号组合 zip merge combinelatest sample concat
-    // 把 stepSignal 和 timer 进行一个 zip,zip 发送的比较晚的控制时间
-//    RACSignal *spiritRunSignal = [[stepSignal zipWith:oneStepClickSignal] reduceEach:^id(RACTuple *xy, id _){
-//        return xy; // 我们要取的值是第一个值 stepSignal 的值，第二个控制时间的并不关心
-//    }];
+    RACSignal *clickSignal = [RACSignal merge:@[autoBtnClickSignal, oneStepClicSignal]];
     
+    clickSignal = [[[clickSignal scanWithStart:@"" reduce:^id(id running, id next) {
+        // 已点击A 又点击 A 则，返回空，否则返回 next
+        if ([running isEqualToString:@"A"] && [next isEqualToString:@"A"]) {
+            return @"";
+        }
+        return next;
+    }] map:^id(id value) {
+        if([value isEqualToString:@"A"]) { return timerSignal;}
+        if([value isEqualToString:@"M"]) { return [RACSignal return:nil];} // nil 任意一个信号，有值的,单点（可以认为占位）
+        return [RACSignal empty]; // 空信号，只alloc init 没有值，
+    }] switchToLatest];
+    
+    // 循环 tag idx
+    RACSignal *runSignal = [clickSignal scanWithStart:@0 reduce:^id(NSNumber *running, id next) {
+        NSInteger idx = running.integerValue;
+        idx++;
+        if (idx == spiritCount) {
+            idx = 1;
+        }
+        return @(idx);
+    }];
+    
+    /**
+     *  结题思路
+     *  1. scan 方向坐标变为 绝对坐标
+     *  2. map delay concat 得到时间间隔为 1 秒的绝对坐标
+     *  3. 封装了一个block 根据idx 得到 移动Tulple
+     *  4. 创建一个timer信号，并把 timer 信号变为数字信号
+     *  5. mapReplace 点击信号，merge两个信号，A & A 返回 空，否则返回 next
+     *  6. map 替换 A 为 1.5 秒 时间信号 替换 M 为单点信号， 否则返回 empty，并 switchToLast降阶
+     *  7. scan clickSignal，变为数字信号， flattenMap 得到 坐标信号。
+     */
+    
+    
+    
+    
+    /*
+    // autoRunBtnSignal
+    RACSignal *autoRunBtnSignal = [self.autoRunBtn rac_signalForControlEvents:UIControlEventTouchUpInside];
+    
+    // oneStepBtnSignal
+    RACSignal *oneStepBtnSignal = [self.oneStepBtn rac_signalForControlEvents:UIControlEventTouchUpInside];
+    
+   // 1s timerSignal
+    RACSignal *oneTimer = [RACSignal interval:1 onScheduler:[RACScheduler mainThreadScheduler]];
+    RACSignal *oneHalfTimer = [RACSignal interval:1.5 onScheduler:[RACScheduler mainThreadScheduler]];
+    
+   // 自动按钮点击后 每1.5 秒产生一个信号，再次点击，停止信号
+    RACSignal *autoRunSignal = [[[autoRunBtnSignal take:1] concat:oneHalfTimer] takeUntil:[autoRunBtnSignal skip:1]];
+    
+    // intervalStepSignal 每 1 秒 挪动一步
+    RACSignal *intervalStepSignal = [[stepSignal zipWith:oneTimer] reduceEach:^id(RACTuple *xy, id _){
+        return xy;
+    }];
+    
+    // 处理 intervalStepSignal 附加 tag status
+    intervalStepSignal = [intervalStepSignal scanWithStart:RACTuplePack(@-1, @0,nil) reduce:^id(RACTuple *info, RACTuple *xy) {
+        NSNumber *idx = info.first;
+        NSInteger intIdx = idx.integerValue + 1;
+        NSNumber *status = @(SpiritStateRunning);
+        return RACTuplePack(@(intIdx), status, xy);
+    }];
+    
+    // autoRunBtnSignal 1.5 秒发送一个 intervalStepSignal，信号的信号高阶信号
+    autoRunSignal = [[autoRunSignal mapReplace:intervalStepSignal] flatten];
+     */
+    
+    // 数字信号flattenMap 变为 移动信号
+    RACSignal *spiritRunSignal = [runSignal flattenMap:newSpiritSignal];
     @weakify(self)
-    [spiritRunSignal subscribeNext:^(RACTuple *xy) {
+    [[spiritRunSignal deliverOnMainThread] subscribeNext:^(RACTuple *info) {
         @strongify(self)
-        RACTupleUnpack(NSNumber *x, NSNumber *y) = xy;
-        CGFloat spiritHeight = self.grid.frame.size.height / GridYBlocks;
-        CGFloat spiritWidth = self.grid.frame.size.width / GridXBlocks;
-        [UIView animateWithDuration:0.5 animations:^{
-            self.spirit.frame = CGRectMake(spiritWidth * x.integerValue, spiritHeight * y.integerValue, spiritWidth, spiritHeight);
-        }];
+        RACTupleUnpack(NSNumber *idx, NSNumber *state, RACTuple *xy) = info;
+        SpiritState stateValue = state.unsignedIntegerValue;
+        NSInteger idxValue = idx.integerValue;
+        UIImageView *spirit = [self.grid viewWithTag:idxValue];
+        
+        switch (stateValue) {
+            case SpiritStateAppear:
+            {
+                updateXYConstraints(spirit, xy);
+                [UIView animateWithDuration:1 animations:^{
+                    spirit.alpha = 1.0f;
+                }];
+                [spirit startAnimating];
+            }
+                break;
+            case SpiritStateRunning:
+            {
+                
+                [UIView animateWithDuration:1 animations:^{
+                    updateXYConstraints(spirit, xy);
+                }];
+            }
+                break;
+            case SpiritStateDisappear:
+            {
+                [UIView animateWithDuration:1 animations:^{
+                    spirit.alpha = 0.0f;
+                } completion:^(BOOL finished) {
+                    [spirit stopAnimating];
+                }];
+                
+            }
+                break;
+            default:
+                break;
+        }
+        
         
     }];
+    
     
     
     // Do any additional setup after loading the view, typically from a nib.
